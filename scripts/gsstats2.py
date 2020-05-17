975c5016bc65831cfb2242aa00553f99eced0f1f
Tualua <32481693+Tualua@users.noreply.github.com>
2020-05-17 21:49:43 +0800

    Update gsstats2.py

--- scripts/gsstats2.py
+++ scripts/gsstats2.py
@@ -10,15 +10,20 @@ def exec_shell_command(command):
     return stdout.split('\n')
 
 
-def get_session_ipaddress(start, end, domainName):
-    data = exec_shell_command("journalctl --since=\"{}\" --until=\"{}\" --no-pager -tgameserver/{} -r "\
-                                "|grep -E \"r: New client address\"".format(start, end, domainName))
-    return data[0].split(' ')[-1].split(':')[0]
+def get_session_ipaddress(data_raw):
+    for line in data_raw:
+        if 'r: New client address' in line:
+            return line.split(' ')[-1].split(':')[0]
+    return ''
 
+def get_session_log(start, end, domainName):
+    return exec_shell_command("journalctl --since=\"{}\" --until=\"{}\" --no-pager -tgameserver/{} -r ".format(start, end, domainName))
 
-def get_fps_stats(start, end, domainName):
-    data = exec_shell_command("journalctl --since=\"{}\" --until=\"{}\" --no-pager -tgameserver/{} -r "\
-                                "|grep -e \"FPS (for last 5 sec)\"| awk '{{print $12}}'".format(start, end, domainName))[:-1]
+def get_fps_stats(data_raw):
+    data = []
+    for line in data_raw:
+        if 'FPS (for last 5 sec)' in line:
+            data.append(int(line.split(' | ')[-1].split(' ')[0]))
     if len(data)>0:
         fps = np.array([int(val) for val in data])
         p99 = np.percentile(fps, 99)
@@ -27,9 +32,11 @@ def get_fps_stats(start, end, domainName):
         return 0
 
 
-def get_latency_stats(start, end, domainName):
-    data = exec_shell_command("journalctl --since=\"{}\" --until=\"{}\" --no-pager -tgameserver/{} -r "\
-                                "|grep -e \"Ping (for last 5 sec)\"| awk '{{print $11}}'".format(start, end, domainName))[:-1]
+def get_latency_stats(data_raw):
+    data = []
+    for line in data_raw:
+        if 'Ping (for last 5 sec)' in line:
+            data.append(int(line.split(': ')[-1].split(' ')[0]))
     if len(data)>0:
         ping = np.array([int(val) for val in data])
         p99 = np.percentile(ping, 99)
@@ -38,24 +45,37 @@ def get_latency_stats(start, end, domainName):
         return 9999
 
 
+def get_resolution(data_raw):
+    for line in data_raw:
+        if 'CreateProcessSync: "python.exe" c:/temp/sc.py' in line:
+            return line.split(' ')[-3:]
+
 def time_difference(time0, time1):
     seconds = (time.mktime(time.strptime(time1, "%Y-%m-%d %H:%M:%S")) - time.mktime(time.strptime(time0, "%Y-%m-%d %H:%M:%S")))
     return int(seconds/60)
 
 def get_location(ipaddress):
     reader = geoip2.database.Reader('/usr/share/GeoIP/GeoLite2-City.mmdb')
-    country, city = '', ''
+    readerasn = geoip2.database.Reader('/usr/share/GeoIP/GeoLite2-ASN.mmdb')
+    result = [''] * 8
     try:
         location = reader.city(ipaddress)
-        country_u = location.country.name
-        country = country_u.encode('ascii', 'replace')
-        city_u = location.city.name
-        if city_u:
-            city = city_u.encode('ascii', 'replace').replace('?','')
+        result[0] = location.continent.name.encode('ascii', 'replace')
+        result[1] = location.country.name.encode('ascii', 'replace')
+        result[2] = location.subdivisions.most_specific.name.encode('ascii', 'replace')
+        result[3] = location.city.name.encode('ascii', 'replace')
+        result[4] = location.location.latitude
+        result[5] = location.location.longitude
+        asn = readerasn.asn(ipaddress)
+        result[6] = asn.autonomous_system_number
+        result[7] = asn.autonomous_system_organization.encode('ascii', 'replace')
     except geoip2.errors.AddressNotFoundError:
         pass
+    except AttributeError:
+        pass
     reader.close()
-    return country, city
+    readerasn.close()
+    return result
 
 def get_servers(path="/usr/local/etc/gameserver/conf.xml"):
     tree = ET.parse(path)
@@ -76,7 +96,7 @@ def get_sessions(domainName):
         session_id = data[i].rsplit(' ')[-4][:-1].strip()
         session_start = data[i].split(' ')[0][:-5].replace('T',' ')
         session_game = data[i+1].split("/")[-1]
-        sessions[session_id] = [session_start,'',session_game]
+        sessions[session_id] = [session_start, '', domainName, session_game]
 
     data = exec_shell_command('journalctl -o short-iso --since=yesterday --no-pager -tgameserver/{} -r ' \
                                 '|grep -E \"StopGameSession\"'.format(domainName))[:-1]
@@ -90,16 +110,20 @@ def get_sessions(domainName):
         session_start = session_data[0]
         session_end = session_data[1]
         if session_end:
-            ipaddress = get_session_ipaddress(session_start, session_end, domainName)
-            country,city = '',''
+            session_log = get_session_log(session_start, session_end, domainName)
+            ipaddress = get_session_ipaddress(session_log)
             if ipaddress:
-                country, city = get_location(ipaddress)
+                location = get_location(ipaddress)
             minutes = time_difference(session_start,session_end)
-            fps_stats = get_fps_stats(session_start, session_end, domainName)
-            latency_stats = get_latency_stats(session_start, session_end, domainName)
-            sessions[session_id].extend([ipaddress, country, city, minutes, fps_stats, latency_stats])
+            fps_stats = get_fps_stats(session_log)
+            latency_stats = get_latency_stats(session_log)
+            resolution = get_resolution(session_log)
+            sessions[session_id].extend([ipaddress])
+            sessions[session_id].extend(location)
+            sessions[session_id].extend([minutes, fps_stats, latency_stats])
+            sessions[session_id].extend(resolution)
         else:
-            sessions[session_id].extend(['', '', '', 0, 0, 0])
+            sessions[session_id].extend(['', '', '', '', '', 0, 0, 0,'','',''])
     return sessions
 
 def get_session_data():
