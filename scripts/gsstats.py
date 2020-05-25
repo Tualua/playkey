482cfaf47b5eea9a143d56a58007898d4e693128
Tualua <32481693+Tualua@users.noreply.github.com>
2020-05-25 12:19:25 +0800

    Update gsstats.py
    Remove -b flag from journalctl command

--- scripts/gsstats.py
+++ scripts/gsstats.py
@@ -29,7 +29,7 @@ def exec_shell_command(command):
 
 #Read GameServer log for <vm> for <start>-<end> timeframe. 
 def get_log(domainName, start, end, reverse=False, debug=False):
-    jctl_options = ["-o short-iso -b"]
+    jctl_options = ["-o short-iso"]
     jctl_options.append("--no-pager -tgameserver/{}".format(domainName))
     
     if start:
