3a1e108c33c2ae1184445b60138e57af32db2516
Tualua <32481693+Tualua@users.noreply.github.com>
2020-08-08 14:02:18 +0800

    Create aria2.js

---
+++ aria2/aria2.js
@@ -0,0 +1,13 @@
+function aria2_download(r) {
+    var token = "token:zDeeVE0nq5Win8YzQxXRfHkCLerHYoaKgfU0kR8DX9A";
+    var diffurl = "http://diff.playkey.net:81" + r.uri;
+    var json_str = JSON.stringify([token, [diffurl]]);
+    var params = json_str.toBytes().toString('base64').split('=').join('%3D');
+    //var aria_uri = "/aria2?method=aria2.addUri&id="+r.variables.request_id+"&params="+params;
+
+    var aria2_args = "method=aria2.addUri&id="+r.variables.request_id+"&params="+params;
+    r.subrequest("/aria2", aria2_args);
+    r.return(404);
+}
+
+export default {aria2_download};
