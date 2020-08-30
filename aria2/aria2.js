a49bfc3c99f44292960b6c45639e0e0186143820
Tualua <32481693+Tualua@users.noreply.github.com>
2020-08-30 14:17:30 +0800

    Update aria2.js

--- aria2/aria2.js
+++ aria2/aria2.js
@@ -1,4 +1,6 @@
-function makeid(length) {
+var token = "token:aaaa";
+
+function make_id(length) {
    var result           = '';
    var characters       = 'abcdef0123456789';
    var charactersLength = characters.length;
@@ -8,67 +10,65 @@ function makeid(length) {
    return result;
 }
 
-function aria_args(token, method, id, params) {
-    var json_str = JSON.stringify([token, params]);
-    var params = json_str.toBytes().toString('base64').split('=').join('%3D');
-    return "method=aria2." + method + "&id=" + id +"&params="+params;
+function make_aria2_args(token, method, id, params) {
+    var params_b64 = JSON.stringify([token, params]).toBytes().toString('base64').split('=').join('%3D');
+    var aria2_method = "aria2." + method;
+    return `method=${aria2_method}&id=${id}&params=${params_b64}`
 }
 
 function aria2_download(r) {
-    var token = "token:aaa";
     var diffurl = "http://diff.playkey.net:81" + r.uri;
-    var aria2_args = aria_args(token, "addUri", r.variables.request_id, [diffurl]);
-    var args_get_downloads = aria_args(token, "tellActive", makeid(16), ['gid', 'status', 'files']);
-    var fileName = r.uri.toString().substring(1);
-    r.subrequest("/aria2", args_get_downloads).then(rDownloads => {
-        var dictDownloads = {};
+    var args_add_uri = make_aria2_args(token, "addUri", r.variables.request_id, [diffurl]);
+    var args_get_downloads = make_aria2_args(token, "tellActive", make_id(16), ['gid', 'status', 'files']);
+    var file_name = r.uri.toString().substring(1);
+    r.subrequest("/aria2", args_get_downloads)
+        .then(res_downloads => {
+            var status_downloads = {};
 
-        function convtodict(value) {
-            var fname1 = value.files[0].uris[0].uri.split('/');
-            var fname2 = fname1[fname1.length - 1];
-            dictDownloads[fname2] = [value.gid, value.status];
-        }
-        var statusDownloads = JSON.parse(rDownloads.responseBody);
-        statusDownloads.result.forEach(convtodict);
-        var checkDownloads = {}
-        checkDownloads['name'] = fileName
-        if (fileName in dictDownloads) {
-            r.error(`File ${fileName} already downloading!`);
-            checkDownloads["gid"] = dictDownloads[fileName][0];
-            checkDownloads["downloading"] = true;
-            if (dictDownloads[fileName][1] == "error") {
-                r.error(`Error in download ${checkDownloads["gid"]}! Filename: ${fileName}`);
-                checkDownloads["error"] = true;
+            function convtodict(value) {
+                var fname1 = value.files[0].uris[0].uri.split('/');
+                var fname2 = fname1[fname1.length - 1];
+                status_downloads[fname2] = [value.gid, value.status];
+            }
+
+            JSON.parse(res_downloads.responseBody).result.forEach(convtodict);
+            var checkDownloads = {}
+            checkDownloads['name'] = file_name
+            if (file_name in status_downloads) {
+                r.error(`File ${file_name} already downloading!`);
+                checkDownloads["gid"] = status_downloads[file_name][0];
+                checkDownloads["downloading"] = true;
+                if (status_downloads[file_name][1] == "error") {
+                    r.error(`Error in download ${checkDownloads["gid"]}! file_name: ${file_name}`);
+                    checkDownloads["error"] = true;
+                } else {
+                    checkDownloads["error"] = false;
+                }
             } else {
+                checkDownloads["downloading"] = false;
                 checkDownloads["error"] = false;
             }
-        } else {
-            checkDownloads["downloading"] = false;
-            checkDownloads["error"] = false;
-        }
-        //r.error(JSON.stringify(checkDownloads, null, 1));
-        return checkDownloads;
-    }, err => {r.error(err.message);}).then(file_status => {
-        //r.error(JSON.stringify(file_status, null, 1))
-        if (file_status["downloading"]) {
-            if (file_status["error"]) {
-                r.error(`Removing download ${file_status["gid"]} from aria2!`);
-                remove_args = aria_args(token, "forceRemove", makeid(16), [file_status["gid"]]);
-                r.subrequest("/aria2", remove_args).then(res => { r.return(404); });
+            return checkDownloads;
+    }, err => {r.error(err.message);})
+        .then(file_status => {
+            if (file_status["downloading"]) {
+                if (file_status["error"]) {
+                    r.error(`Removing download ${file_status["gid"]} from aria2!`);
+                    remove_args = make_aria2_args(token, "forceRemove", make_id(16), [file_status["gid"]]);
+                    r.subrequest("/aria2", remove_args).then(res => { r.return(404); });
+                } else {
+                    r.return(404);
+                }
             } else {
-                r.return(404);
+                r.error(`File ${file_name} not found! Creating download in aria2!`);
+                r.subrequest("/aria2", args_add_uri).then(res => { r.return(404); });
             }
-        } else {
-            r.error(`File ${fileName} not found! Creating download in aria2!`);
-            r.subrequest("/aria2", aria2_args).then(res => { r.return(404); });
-        }
     })
 }
 
 
 function aria2_status(r) {
-    var token = "token:aaaa";
-    var args_get_downloads = aria_args(token, "tellActive", makeid(16), ['gid', 'status', 'files'])
+    var args_get_downloads = make_aria2_args(token, "tellActive", make_id(16), ['gid', 'status', 'files'])
     var status = r.subrequest("/aria2", args_get_downloads).then(function(res) {
         function getgid(value) {
                 var fileuri = value.files[0].uris[0].uri.split('/');
