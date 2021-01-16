b4f5e910b5d8606e6c36996a7b5ee1af7088d9df
Tualua <32481693+Tualua@users.noreply.github.com>
2021-01-16 16:39:45 +0800

    Delete pkmsvc directory

--- pkmsvc/PKM_PrepareDisk.ps1
+++
@@ -1,4 +0,0 @@
-$ToolsDrive = Get-Volume -FileSystemLabel Tools
-$ToolsLetter = $ToolsDrive.DriveLetter
-$MinerPath = $ToolsLetter + ":\gminer\"
-Copy-Item $MinerPath -Destination 'C:\Temp\gminer\' -Recurse
